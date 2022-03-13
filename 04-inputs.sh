d5e17db8bf5b5f59ebbcc3acac352a1ba7cb684e
Ravi J <ravikiranreddy.j@gmail.com>
2022-03-13 17:25:45 +0530

    Input Read Command

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -2,3 +2,10 @@
 
 read -p 'Enter your Name: ' name
 echo "your name = $name"
+
+# Special Variables
+# $0-$n , $* / $@. $#
+echo Script name = $0
+echo First Argument = $1
+echo All Arguments = $*
+echo number of Arguments = $#
