fecc3d5b7c8aa6070569b118f28f712be06139c3
Ravi J <ravikiranreddy.j@gmail.com>
2022-03-13 20:45:49 +0530

    Conditional commands

---
+++ 06-condition.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# case and if are conditional commands, if command is widely used because it has more options than case command
+
+# If Condition
+# If Found in Three forms
+
+# simple if
+# if [ expression ]
+# then
+# command
+# fi
+
+ if [ 1 -eq 1 ]
+ then
+   echo Hello
+ fi
---
+++ 06-exit-status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# exit status is a number, $? is a special variable that holds this value
+# $? will have the exit status of the last executed command
+
+# Range of exit status is 0-255
+# 0 -> Global Success
+# 1-125 -> Some failure from the command
+# 125+ -> System Failure
+
